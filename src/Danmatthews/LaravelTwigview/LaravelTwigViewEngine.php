1e5290c8dc866ad8d57477c4811e65011732615b
Dan Matthews <dan@danmatthews.me>
2013-01-16 22:27:04 +0000

    Uses app storage for Twig cache files now

--- src/Danmatthews/LaravelTwigview/LaravelTwigViewEngine.php
+++ src/Danmatthews/LaravelTwigview/LaravelTwigViewEngine.php
@@ -17,7 +17,9 @@ class LaravelTwigViewEngine implements \Illuminate\View\Engines\EngineInterface 
 		$loader = new Twig_Loader_String();
 
 		// Load an environment object for this loader.
-		$twig = new Twig_Environment($loader);
+		$twig = new Twig_Environment($loader, array(
+		   'cache' => base_path().'/app/storage/views',
+	    ));
 
 		// Add the url() function as the base method.
 		$twig->addFunction(new Twig_SimpleFunction('url', 'URL::to'));
