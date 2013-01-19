200bc5f66c8d4126adf628abfc8e02b717deed02
Dan Matthews <dan@danmatthews.me>
2013-01-19 01:47:13 +0000

    Work on getting the include() function to work

--- src/Danmatthews/LaravelTwigview/Engines/LaravelTwigViewEngine.php
+++ src/Danmatthews/LaravelTwigview/Engines/LaravelTwigViewEngine.php
@@ -1,55 +1,72 @@
-<?php namespace Danmatthews\LaravelTwigView\Engines;
+<?php namespace Danmatthews\LaravelTwigview\Engines;
 
 use Twig_Loader_Filesystem;
 use Twig_Environment;
 use Twig_SimpleFunction;
-use Danmatthews\LaravelTwigview\Loaders\LaravelTwigLoader;
 
-class LaravelTwigViewEngine implements \Illuminate\View\Engines\EngineInterface {
+class LaravelTwigViewEngine implements \Illuminate\View\Engines\EngineInterface
+{
+    /**
+     * Store the application instance.
+     * @var object
+     */
+    public $app;
 
-	/**
-	 * Store the application instance.
-	 * @var object
-	 */
-	public $app;
+    public function __construct($app)
+    {
+        $this->app = $app;
+    }
 
-	public function __construct($app) {
-		$this->app = $app;
-	}
+    /**
+     * Get the evaluated contents of the view.
+     *
+     * @param  string $path
+     * @param  array  $data
+     * @return string
+     */
+    public function get($path, array $data = array())
+    {
 
-	/**
-	 * Get the evaluated contents of the view.
-	 *
-	 * @param  string  $path
-	 * @param  array   $data
-	 * @return string
-	 */
-	public function get($path, array $data = array())
-	{
-		// Get the directory name for this template.
-		$viewdir = dirname($path);
+        // Get the list of view paths from the app.
+        $paths = $this->app['view']->getFinder()->getPaths();
 
-		// Get the path to JUST the template.
-		$path = str_replace($viewdir.'/', '', $path);
+        // Sort the paths, longest first.
+        usort(
+            $paths,
+            function ($a, $b) {
+                return strlen($b)-strlen($a);
+            }
+        );
 
-		// Create a loader for this template.
-		$loader = new Twig_Loader_Filesystem($viewdir);
+        // Get the directory the requested view sits in.
+        $viewdir = dirname($path);
 
-		// Load an environment object for this loader.
-		$twig = new Twig_Environment($loader, array(
-		   'cache' => base_path().'/app/storage/views',
-		   'autoescape' => false,
-		   'auto_reload' => true,
-	    ));
+        // Match it to a view path registered in config::view.paths
+        foreach ($paths as $dir) {
+            if (stristr($viewdir, $dir)) {
+                $path = str_replace($dir.'/', '', $path);
+                break;
+            }
+        }
 
-		// Add the url() function as the base method.
-		$twig->addFunction(new Twig_SimpleFunction('url', 'URL::to'));
+        // Create a loader for this template.
+        $loader = new Twig_Loader_Filesystem($paths);
 
-		// Add the asset() function for templates.
-		$twig->addFunction(new Twig_SimpleFunction('asset', 'URL::asset'));
+        // Load an environment object for this loader.
+        $twig = new Twig_Environment($loader, array(
+           'cache' => base_path().'/app/storage/views',
+           'autoescape' => false,
+           'auto_reload' => true,
+        ));
 
-		// Render and return the file.
-		return $twig->render($path, $data);
-	}
+        // Add the url() function as the base method.
+        $twig->addFunction(new Twig_SimpleFunction('url', 'URL::to'));
+
+        // Add the asset() function for templates.
+        $twig->addFunction(new Twig_SimpleFunction('asset', 'URL::asset'));
+
+        // Render and return the file.
+        return $twig->render($path, $data);
+    }
 
 }
--- src/Danmatthews/LaravelTwigview/LaravelTwigviewServiceProvider.php
+++ src/Danmatthews/LaravelTwigview/LaravelTwigviewServiceProvider.php
@@ -2,36 +2,38 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class LaravelTwigviewServiceProvider extends ServiceProvider {
+class LaravelTwigviewServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('danmatthews/laravel-twigview');
+        $this->app['view']->addExtension(
+            'twig',
+            'twig',
+            function () {
+                return new Engines\LaravelTwigViewEngine($this->app);
+            }
+        );
+    }
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('danmatthews/laravel-twigview');
-		$this->app['view']->addExtension('twig', 'twig', function()
-		{
-			return new Engines\LaravelTwigViewEngine($this->app);
-		});
-	}
-
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-	}
-
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
 }
